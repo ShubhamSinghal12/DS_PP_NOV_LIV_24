a78f5a2b525629ffa8514d2127270f74370a62da
unknown <shubhamsinghal12@gmail.com>
2024-12-27 20:47:58 +0530

    Basic

---
+++ Python-Intermediate/.ipynb_checkpoints/calculator-checkpoint.py
@@ -0,0 +1,28 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+
+class A:
+    def __init__(self,x):
+        self.x = x
+
+
+
+C = 10
+X = 100
+CONSTANT = 200
+Y = 1000
+
+if __name__ == "__main__":
+    print("Hi I am calling from source")
+else:
+    print("Hi I am calling From calculator Module")
